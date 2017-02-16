0bcf2bd017ead53141ad667023c9781bf5ef3de8
Austin Graham <austingraham731@gmail.com>
2017-02-16 11:08:52 -0600

    Added API function for getting all lems

--- backend/run.py
+++ backend/run.py
@@ -2,6 +2,7 @@ from flask_globals import *
 from flask import request
 from db.leml import Lem
 from mongoengine import *
+import json
 
 init()
 application = get_global_app()
@@ -15,6 +16,16 @@ def lem():
 		obj = lem.to_json()
 	db.close()
 	return obj
+
+#URL for getting all current lem objects in the database
+@app.route('/lemall', methods = ['GET', 'POST'])
+def lemall():
+	db = connect('temp')
+	allobj = []
+	for lem in Lem.objects:
+		allobj.append(lem.to_json())
+	db.close()
+	return json.dumps(allobj)
 	
 #Start the application
 if __name__ == '__main__':
