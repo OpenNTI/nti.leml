38613862a49bb68ba64a66ca96fa1e7850f9e990
Nick Graham <nickbgraham83@gmail.com>
2017-02-16 14:38:39 -0600

    Removes old canvas file for vis and updates for cytoscape

--- js/canvas.js
+++ js/canvas.js
@@ -1,31 +1,42 @@
-// create an array with nodes
-var nodes = new vis.DataSet([
-  {id: 1, label: 'Node 1'},
-  {id: 2, label: 'Node 2'},
-  {id: 3, label: 'Node 3'},
-  {id: 4, label: 'Node 4'},
-  {id: 5, label: 'Node 5'},
-  {id: 6, label: 'Node 6'},
-  {id: 7, label: 'Node 7'},
-  {id: 8, label: 'Node 8'}
-]);
+var cy = cytoscape({
 
-// create an array with edges
-var edges = new vis.DataSet([
-  {from: 1, to: 8, arrows:'to'},
-  {from: 1, to: 3, arrows:'to'},
-  {from: 1, to: 2, arrows:'to, from'},
-  {from: 2, to: 4, arrows:'to'},
-  {from: 2, to: 5, arrows:'to, from'},
-  {from: 5, to: 6, arrows:{to:{scaleFactor:2}}},
-  {from: 6, to: 7, arrows:{middle:{scaleFactor:0.5},from:true}}
-]);
+  container: document.getElementById('cy'), // container to render in
 
-// create a network
-var container = document.getElementById('mynetwork');
-var data = {
-  nodes: nodes,
-  edges: edges
-};
-var options = {};
-var network = new vis.Network(container, data, options);
+  elements: [ // list of graph elements to start with
+    { // node a
+      data: { id: 'a' }
+    },
+    { // node b
+      data: { id: 'b' }
+    },
+    { // edge ab
+      data: { id: 'ab', source: 'a', target: 'b' }
+    }
+  ],
+
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        'label': 'data(id)'
+      }
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle'
+      }
+    }
+  ],
+
+  layout: {
+    name: 'grid',
+    rows: 1
+  }
+
+});
