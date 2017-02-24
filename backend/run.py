71d88daeb71f81c416280254900bc71d181e91ee
Austin Graham <austingraham731@gmail.com>
2017-02-24 09:51:26 -0600

    Added support for persistent database

--- backend/run.py
+++ backend/run.py
@@ -6,13 +6,14 @@ import json
 
 init()
 application = get_global_app()
-db_name = 'temp'
+host = 'mongodb://austinpgraham:lemldb@ds145289.mlab.com:45289/lemlcapstone'
+name = 'leml'
 
 #URL for getting a lem item
 @app.route('/lem', methods = ['GET', 'POST'])
 def lem():
-	db = connect(db_name)
-	obj = "Nothing bud"
+	db = connect(name,host=host)
+	obj = "error"
 	for lem in Lem.objects(lem_id = request.args.get('id')):
 		obj = lem.to_json()
 	db.close()
@@ -21,7 +22,7 @@ def lem():
 #URL for getting all current lem objects in the database
 @app.route('/lemall', methods = ['GET', 'POST'])
 def lemall():
-	db = connect(db_name)
+	db = connect(name,host=host)
 	allobj = []
 	for lem in Lem.objects:
 		allobj.append(lem.to_json())
@@ -33,7 +34,8 @@ def lemall():
 def save():
 	'''json_string = '{"lem_id": 45, "startIDs": [1], "stopIDs": [3], "building_blocks": [{"id": 1, "block_type": "Information", "description": "Demonstration", "method": "Video"}, {"id": 2, "block_type": "Practice", "description": "Application Exercise", "method": "Assignment"}, {"id": 3, "block_type": "Dialogue", "description": "Lessons Learned", "method": "Discussion"}], "contexts": [{"id": 4, "context_type": "Online Asynchronous", "building_blocks": [1, 2, 3], "notations": []}], "actions": [{"id": 5, "action_type": "Learner Action", "source": 1, "target": 2}, {"id": 6, "action_type": "Learner Action", "source": 2, "target": 3}], "notations": []}'''
 	json_string = request.args.get('obj')
-	db = connect(db_name)	
+	#db = connect(db_name)	
+	db = connect(name, host = host)
 	toLem(json_string).save()
 	db.close()
 	return "complete"
