cb24436f112a1fd3270cd00ceb6879b07e1cbb80
smcgunigal <smcgunigal@gmail.com>
2017-02-27 15:07:36 -0600

    Fixed notation actions

--- js/render.js
+++ js/render.js
@@ -245,7 +245,7 @@ function loadNewCytoscapeWith(elements) {
     var nodes = cy.json().elements.nodes;
     nodes.map(function(val) {
       if (val.selected) {
-        if (val.classes.includes("buildingBlock") || val.classes.includes("startstop")) {
+        if (val.classes.includes("buildingBlock") || val.classes.includes("startstop") || val.classes.includes("notation")) {
             if (evt.cyTarget.id() != val.data.id) {
               cy.add([{group: "edges", data: {id: new_id, action_type: "Learner Action", source: val.data.id, target: evt.cyTarget.id()}, classes: "Learner_Action"}]);
               new_id = new_id + 1;
