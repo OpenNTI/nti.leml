1770a8962d58d9a69cc8db1335cb4180054e8b22
Nick Graham <nickbgraham83@gmail.com>
2017-03-01 10:07:34 -0600

    Splits index.html into parts

---
+++ js/load_html.js
@@ -0,0 +1,7 @@
+$(function(){
+  $("#navbar").load("../html/navbar.html");
+  $("#drag_and_drop_pane").load("../html/drag_and_drop_pane.html");
+  $("#sidebars").load("../html/sidebars.html");
+  $("#hidden_functional_elements").load("../html/hidden_functional_elements.html");
+  $("#modal").load("../html/modal.html");
+});
