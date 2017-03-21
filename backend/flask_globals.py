5d91614040c9edb262c5cce35c41af8232d57714
Jared Bond <jaredbond@att.net>
2017-03-21 11:44:43 -0500

    added login stuff

--- backend/flask_globals.py
+++ backend/flask_globals.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_login import LoginManager, login_user, logout_user, UserMixin
+from flask_login import LoginManager, login_user, logout_user, UserMixin, current_user
 from flask_bcrypt import Bcrypt
 from mongoengine import *
 import os
