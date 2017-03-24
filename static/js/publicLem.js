cc368b7985bb7d9e44fc061a04eadd25c35a4685
Nick Graham <nickbgraham83@gmail.com>
2017-03-23 23:26:22 -0500

    Uses one row for public lems

--- static/js/publicLem.js
+++ static/js/publicLem.js
@@ -11,10 +11,10 @@ $(function() {
       lemDivs += generateLemRow("../static/img/templates/absorbDoConnect.png", lem.created_by);
     }
 
-    lemSection.html(lemDivs);
+    lemSection.html('<div class="row">' + lemDivs + '</div>');
   })
 })
 
 function generateLemRow(imgURL, username) {
-  return '<div class="row"><div class="col-sm-6 col-md-4"><div class="thumbnail"><img src=' + imgURL + '><div class="caption"><h3>Absorb Do Connect</h3><p>Created by @'+ username + '</p><p><a href="#" class="btn btn-primary" role="button">Add to Canvas</a></p></div></div></div></div>'
+  return '<div class="col-sm-6 col-md-4"><div class="thumbnail"><img src=' + imgURL + '><div class="caption"><h3>Absorb Do Connect</h3><p>Created by @'+ username + '</p><p><a href="#" class="btn btn-primary" role="button">Add to Canvas</a></p></div></div></div>'
 }
