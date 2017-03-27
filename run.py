12c6f87780a2f257d1f36a225b9bd38f0ec124a0
Nick Graham <nickbgraham83@gmail.com>
2017-03-27 18:15:53 -0500

    Upload works through form

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from flask_globals import *
-from flask import request, render_template
+from flask import render_template, Flask, request, redirect, url_for, send_from_directory
 from db.leml import Lem, toLem
 from db.user import User as DBUser
 from mongoengine import *
 import json
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 init()
 application = get_global_app()
@@ -11,6 +14,10 @@ login_manager = get_login_manager()
 host = 'mongodb://austinpgraham:lemldb@ds145289.mlab.com:45289/lemlcapstone'
 name = 'leml'
 in_use_id = [0]
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #URL for getting a lem item
 @app.route('/lem', methods = ['GET'])
@@ -21,6 +28,39 @@ def lem():
 		obj = lem.to_json()
 	db.close()
 	return obj
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input id="fileinput" type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 #URL for getting all current lem objects in the database
 @app.route('/lemall', methods = ['GET'])
@@ -114,7 +154,7 @@ def logout():
 @app.route('/public')
 def public():
 	return render_template("public.html")
-	
+
 @app.route('/')
 def home():
 	return render_template("index.html")
