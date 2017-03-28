0f26fca4b385b10a09b173a93dd6025b7884b89a
Sean McGunigal <smcgunigal@gmail.com>
2017-03-28 15:30:27 -0500

    Button to add to canvas

--- static/js/publicLem.js
+++ static/js/publicLem.js
@@ -9,27 +9,25 @@ $(function() {
       var lem = JSON.parse(lems[lemIndex]);
 
       var imgURL = lem.thumbnail;
+      var id = lem._id.$oid;
 
       // default img
       if (!imgURL) {
         imgURL = "../static/img/templates/absorbDoConnect.png";
       }
 
-      lemDivs += generateLemRow(lem.name, lem.created_by, imgURL);
+      lemDivs += generateLemRow(lem.name, lem.created_by, imgURL, id);
     }
 
     lemSection.html('<div class="row">' + lemDivs + '</div>');
   })
 })
 
-function generateLemRow(title, username, imgURL) {
-  return '<div class="col-sm-6 col-md-4"><div class="thumbnail"><img src=' + imgURL + '><div class="caption"><h3>' + title + '</h3><p>Created by @'+ username + '</p><p><a href="#" class="btn btn-primary" role="button" onclick="dosomething(this.parentElement.parentElement);">Add to Canvas</a></p></div></div></div>';
+function generateLemRow(title, username, imgURL, id) {
+  return '<div class="col-sm-6 col-md-4"><div class="thumbnail"><img src=' + imgURL + '><div id="' + id + '" class="caption"><h3>' + title + '</h3><p>Created by @'+ username + '</p><p><a href="#" class="btn btn-primary" role="button" onclick="dosomething(this.parentElement.parentElement);">Add to Canvas</a></p></div></div></div>';
 }
-<<<<<<< HEAD
 
 function dosomething(test) {
-  console.log('something');
-  console.log(test);
+  var t = $.get("http://localhost:5000/lem", {"id": test.id});
+  console.log(t);
 }
-=======
->>>>>>> frontend
