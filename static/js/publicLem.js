7adcc7c5dda304d35bc6f94bf735284fe9842be1
Sean McGunigal <smcgunigal@gmail.com>
2017-03-29 13:27:02 -0500

    Added search bar

--- static/js/publicLem.js
+++ static/js/publicLem.js
@@ -18,13 +18,13 @@ $(function() {
 
       lemDivs += generateLemRow(lem.name, lem.created_by, imgURL, id);
     }
-
-    lemSection.html('<div class="row">' + lemDivs + '</div>');
+    var str_test = '<div class="row"><div class="col-lg-6"><div class="input-group"><span class="input-group-btn"><button class="btn btn-default" type="button" onclick="searchLems();">Search</button></span><input id="search_field" type="text" class="form-control" placeholder="Search for..."></div><!-- /input-group --></div><!-- /.col-lg-6 --></div>';
+    lemSection.html(str_test + '<div class="row">' + lemDivs + '</div>');
   })
 })
 
 function generateLemRow(title, username, imgURL, id) {
-  return '<div class="col-sm-6 col-md-4"><div class="thumbnail"><img src=' + imgURL + '><div id="' + id + '" class="caption"><h3>' + title + '</h3><p>Created by @'+ username + '</p><p><a href="#" class="btn btn-primary" role="button" onclick="addToCanvas(this.parentElement.parentElement);">Add to Canvas</a></p></div></div></div>';
+  return '<div class="col-sm-6 col-md-4 lems"><div class="thumbnail"><img src=' + imgURL + '><div id="' + id + '" class="caption"><h3>' + title + '</h3><p>Created by @'+ username + '</p><p><a href="#" class="btn btn-primary" role="button" onclick="addToCanvas(this.parentElement.parentElement);">Add to Canvas</a></p></div></div></div>';
 }
 
 function addToCanvas(test) {
@@ -37,4 +37,16 @@ function addToCanvas(test) {
   redraw();
   //console.log(t);
   //renderLem(t.responseText);
+}
+
+function searchLems() {
+  var searchValue = $('#search_field').val();
+
+  $(".lems").each(function(){
+    if($(this).html().toLowerCase().indexOf(searchValue.toLowerCase()) > -1){
+     $(this).removeClass('hidden');
+    } else {
+      $(this).addClass('hidden');
+    }
+  });
 }
