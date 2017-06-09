97c59a351f32e61fa139f206ad5fb4997410d2c6
austingraham731@gmail.com <austin.graham@AustinGraham.local>
2017-06-09 11:08:18 -0500

    to checkout master

--- run.py
+++ run.py
@@ -12,7 +12,10 @@ import sys
 from flask_api import status
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-DEFAULT_FAVORITES = ["58f50621cf367e67548e0e80","58f50667cf367e67548e0e81", "58f50696cf367e67548e0e82"]
+DEFAULT_FAVORITES = [
+    "58f50621cf367e67548e0e80",
+    "58f50667cf367e67548e0e81",
+    "58f50696cf367e67548e0e82"]
 
 init()
 application = get_global_app()
@@ -21,6 +24,8 @@ login_manager = get_login_manager()
 mongo = {}
 
 # URL for getting a lem item
+
+
 @app.route('/lem', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def lem():
     if request.method == 'GET':
@@ -29,14 +34,16 @@ def lem():
     if current_user.is_authenticated:
         data = request.get_json(force=True)
         if request.method == 'DELETE':
-            db = connect(db=mongo["name"], 
+            db = connect(db=mongo["name"],
                          host=mongo["host"],
                          username=mongo["username"],
                          password=mongo["password"])
             for lem in Lem.objects(pk=ObjectId(data['id'])):
                 if lem.created_by.email != current_user.email:
-                    return "Current user (" + current_user.email + ") does not own lem: " + data['id'], status.HTTP_401_UNAUTHORIZED
-            for fave in User_Favorite_Lems.objects(favorites=ObjectId(data['id'])):
+                    return "Current user (" + current_user.email + ") does not own lem: " + \
+                        data['id'], status.HTTP_401_UNAUTHORIZED
+            for fave in User_Favorite_Lems.objects(
+                    favorites=ObjectId(data['id'])):
                 fave.update(pull__favorites=ObjectId(data['id']))
             db.close()
             return delete(ObjectId(data['id']), name, host)
@@ -44,12 +51,14 @@ def lem():
     return login_manager.unauthorized()
 
 # URL for getting all current lem objects in the database
+
+
 @app.route('/lemall', methods=['GET'])
 def lemall():
-    db = connect(db=mongo["name"], 
-                         host=mongo["host"],
-                         username=mongo["username"],
-                         password=mongo["password"])
+    db = connect(db=mongo["name"],
+                 host=mongo["host"],
+                 username=mongo["username"],
+                 password=mongo["password"])
     allobj = []
     for lem in Lem.objects(public=1):
         allobj.append(lem.to_json())
@@ -57,13 +66,15 @@ def lemall():
     return json.dumps(allobj)
 
 # URL for getting user lems
+
+
 @app.route('/lemuser', methods=['GET'])
 @login_required
 def lemuser():
-    db = connect(db=mongo["name"], 
-                         host=mongo["host"],
-                         username=mongo["username"],
-                         password=mongo["password"])
+    db = connect(db=mongo["name"],
+                 host=mongo["host"],
+                 username=mongo["username"],
+                 password=mongo["password"])
     allobj = []
     for lem in Lem.objects(created_by=current_user.email):
         allobj.append(lem.to_json())
@@ -78,11 +89,11 @@ def register():
     usr_name = data['email']
     password = data['pass']
     pwd_hash = getHash(password)
-    db = connect(db=mongo["name"], 
-                         host=mongo["host"],
-                         username=mongo["username"],
-                         password=mongo["password"])
-    if  DBUser.objects(pk = usr_name).count() > 0:
+    db = connect(db=mongo["name"],
+                 host=mongo["host"],
+                 username=mongo["username"],
+                 password=mongo["password"])
+    if DBUser.objects(pk=usr_name).count() > 0:
         return "Email already exists", status.HTTP_400_BAD_REQUEST
     DBUser(usr_name, pwd_hash).save()
     User_Favorite_Lems(usr_name, DEFAULT_FAVORITES).save()
@@ -137,14 +148,15 @@ def logout():
 def home():
     return render_template("index.html")
 
-@app.route('/comment', methods = ['GET', 'POST'])
+
+@app.route('/comment', methods=['GET', 'POST'])
 def comment():
     if request.method == 'GET':
         lem_id = request.args.get('lem')
         comments = []
-        for comment in Comment.objects(lem_id = lem_id):
+        for comment in Comment.objects(lem_id=lem_id):
             include = True
-            for lem in Lem.objects(pk = lem_id):
+            for lem in Lem.objects(pk=lem_id):
                 if lem.public == 0 and lem.created_by.email != current_user.email:
                     include = False
                     break
@@ -158,24 +170,28 @@ def comment():
             text = data["text"]
             created_by = current_user.email
 
-            db = connect(db=mongo["name"], 
+            db = connect(db=mongo["name"],
                          host=mongo["host"],
                          username=mongo["username"],
                          password=mongo["password"])
 
             # Check that a private lem is not being accessed
-            for lem in Lem.objects(pk = lem_id):
+            for lem in Lem.objects(pk=lem_id):
                 if lem.public == 0 and lem.created_by.email != current_user.email:
                     return "Cannot comment on a private lem not owned by you", status.HTTP_403_FORBIDDEN
 
             resultComment = {}
-            for lem in Lem.objects(pk = lem_id):
-                comment = Comment(lem_id = str(lem_id), text = text, created_by = created_by)
+            for lem in Lem.objects(pk=lem_id):
+                comment = Comment(
+                    lem_id=str(lem_id),
+                    text=text,
+                    created_by=created_by)
                 comment.save()
                 resultComment = comment.to_json()
             db.close()
             return resultComment
         return login_manager.unauthorized()
+
 
 @app.route('/rate', methods=['POST'])
 @login_required
@@ -184,27 +200,29 @@ def rate():
     new_rating = float(data["rating"])
     lem_id = ObjectId(data["lem"])
     new_avg = 0
-    rating_item = Ratings.objects.filter(Q(lem = lem_id) & Q(user = current_user.email))
+    rating_item = Ratings.objects.filter(
+        Q(lem=lem_id) & Q(user=current_user.email))
     if len(rating_item) == 0:
-        Ratings(user = current_user.email, lem = lem_id, rating = new_rating).save()
+        Ratings(user=current_user.email, lem=lem_id, rating=new_rating).save()
     else:
-        rating_item[0].update(rating = new_rating)
-    ratings = Ratings.objects(lem = lem_id)
+        rating_item[0].update(rating=new_rating)
+    ratings = Ratings.objects(lem=lem_id)
     new_avg = sum(r.rating for r in ratings) / len(ratings)
-    print(Lem.objects(pk = lem_id))
-    Lem.objects(pk = lem_id).update(avgRating = new_avg)
+    print(Lem.objects(pk=lem_id))
+    Lem.objects(pk=lem_id).update(avgRating=new_avg)
     return '{"new_avg":"' + str(new_avg) + '", "lem_id":"' + str(lem_id) + '"}'
+
 
 @app.route('/favorite', methods=['GET', 'PUT', 'DELETE'])
 @login_required
 def favorite():
-    db = connect(db=mongo["name"], 
-                         host=mongo["host"],
-                         username=mongo["username"],
-                         password=mongo["password"])
+    db = connect(db=mongo["name"],
+                 host=mongo["host"],
+                 username=mongo["username"],
+                 password=mongo["password"])
 
     id = ObjectId(request.args.get('id'))
-    for lem in Lem.objects(pk = id):
+    for lem in Lem.objects(pk=id):
         favoritedLem = lem
 
     # Create favorite entry for this user if there isn't one
@@ -215,9 +233,14 @@ def favorite():
         User_Favorite_Lems(current_user.email, DEFAULT_FAVORITES).save()
 
     if request.method == 'DELETE':
-        User_Favorite_Lems.objects(pk=current_user.email).update(pull__favorites=favoritedLem)
+        User_Favorite_Lems.objects(
+            pk=current_user.email).update(
+            pull__favorites=favoritedLem)
     if request.method == 'PUT':
-        User_Favorite_Lems.objects(pk=current_user.email).update(add_to_set__favorites=favoritedLem, upsert = True)
+        User_Favorite_Lems.objects(
+            pk=current_user.email).update(
+            add_to_set__favorites=favoritedLem,
+            upsert=True)
 
     # else if GET, also return new favorites on PUT and DELETE
     new_favs = []
@@ -231,10 +254,10 @@ def favorite():
 
 @login_manager.user_loader
 def load_user(id, remember=True):
-    db = connect(db=mongo["name"], 
-                         host=mongo["host"],
-                         username=mongo["username"],
-                         password=mongo["password"])
+    db = connect(db=mongo["name"],
+                 host=mongo["host"],
+                 username=mongo["username"],
+                 password=mongo["password"])
     for user in DBUser.objects(pk=id):
         return User(user.email, user.password)
     return None
@@ -246,13 +269,21 @@ def validate_json(json_dict):
         return False
     return True
 
+
 def _print_help():
     print("\t[-p]\t[host port]\n\t\tThe host/port where the server will run. Default is local host port 5000.")
     print("\t[-d]\t[hostname dbname username password]\n\t\tDatabase connection parameters. If these are not supplied, /db/config.py file will be used.")
-    
+
+
 def _write_to_config(parameters):
     file = open("db/config.py", "w")
-    file.write('mongodb = {"host":"%s", "name": "%s", "username": "%s", "password": "%s"}' % (parameters["host"], parameters["name"], parameters["username"], parameters["password"]))
+    file.write(
+        'mongodb = {"host":"%s", "name": "%s", "username": "%s", "password": "%s"}' %
+        (parameters["host"],
+         parameters["name"],
+         parameters["username"],
+         parameters["password"]))
+
 
 # Start the application
 if __name__ == '__main__':
@@ -262,11 +293,11 @@ if __name__ == '__main__':
     if "-d" in sys.argv:
         start_arg = sys.argv.index("-d")
         try:
-            mongo["host"] = sys.argv[start_arg+1]
-            mongo["name"] = sys.argv[start_arg+2]
-            mongo["username"] = sys.argv[start_arg+3]
-            mongo["password"] = sys.argv[start_arg+4]
-        except:
+            mongo["host"] = sys.argv[start_arg + 1]
+            mongo["name"] = sys.argv[start_arg + 2]
+            mongo["username"] = sys.argv[start_arg + 3]
+            mongo["password"] = sys.argv[start_arg + 4]
+        except Exception:
             _print_help()
             sys.exit()
         _write_to_config(mongo)
@@ -274,7 +305,7 @@ if __name__ == '__main__':
         try:
             from db.config import mongodb
             mongo = mongodb
-        except:
+        except Exception:
             _print_help()
             sys.exit()
     if "-p" in sys.argv:
@@ -282,7 +313,7 @@ if __name__ == '__main__':
         try:
             host = sys.argv[start_arg + 1]
             port = int(sys.argv[start_arg + 2])
-        except:
+        except Exception:
             _print_help()
             sys.exit()
         application.run(debug=True, host=host, port=port)
